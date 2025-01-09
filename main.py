fd8031db02d43e2212467a829b7206f0116139ba
Prem <kumarprem10694@gmail.com>
2025-01-09 12:01:47 +0530

    model train

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage1_data_ingestion import DataIngestionPipeline
 from textSummarizer.pipeline.stage2_data_validation import DataValidationPipeline
 from textSummarizer.pipeline.stage3_data_transformation import DataTransformationPipeline
-
-
-
-
+from textSummarizer.pipeline.stage4_model_trainer import ModelTrainerPipeline
 
 
 STAGE_NAME = "Data imgestion stage "
@@ -16,7 +13,7 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n")
 except Exception as e:
     logger.exception(e)
-    raise e    
+        
 
 
 STAGE_NAME = "Data Validation stage "
@@ -27,7 +24,7 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n")
 except Exception as e:
     logger.exception(e)
-    raise e    
+       
 
 
 STAGE_NAME = "Data transformation stage "
@@ -38,4 +35,16 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n")
 except Exception as e:
     logger.exception(e)
-    raise e    
+    raise e
+        
+
+
+STAGE_NAME = "Model Trainer stage "
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} Started <<<<<<<")
+    Model_trainer = ModelTrainerPipeline()
+    Model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)  
+    raise e
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml , create_directories
-from textSummarizer.entity import (dataIngestionConfig , DataValidationConfig , DataTransformationConfig)
+from textSummarizer.entity import (dataIngestionConfig , DataValidationConfig , DataTransformationConfig , ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -55,3 +55,27 @@ class ConfigurationManager:
         )
         
         return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        param = self.param.TrainingArguments
+        
+        create_directories([config.root_dir])
+        
+        model_trainer_config = ModelTrainerConfig(
+                root_dir = config.root_dir,
+                data_path  = config.data_path,
+                model_ckpt  = config.model_ckpt,
+                num_train_epochs = param.num_train_epochs ,
+                warmup_steps = param.warmup_steps ,
+                per_device_train_batch_size = param.per_device_train_batch_size  ,
+                per_device_eval_batch_size = param.per_device_eval_batch_size ,
+                weight_decay = param.weight_decay ,
+                logging_steps = param.logging_steps ,
+                evaluation_strategy = param.evaluation_strategy  ,
+                eval_steps = param.eval_steps ,
+                save_steps = param.save_steps ,
+                gradient_accumulation_steps = param.gradient_accumulation_steps 
+            )
+        
+        return model_trainer_config
